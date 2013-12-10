04f9e11fd7f2195b227b57133de3b8d8f81dbc55
gracyeli <gracyelisouza@hotmail.com>
2013-12-10 16:06:37 -0300

    Commit inicial

--- conecta.php
+++ conecta.php
@@ -1,37 +1,54 @@
-<?php
-        
-        include ("con.php");
-        
-        $ponto = $_POST['idPonto'];
-        $datainicio = $_POST['datainicio'];
-        $horainicio = $_POST['horainicio'];
-        $datafinal = $_POST['datafinal'];
-        $horafinal = $_POST['horafinal'];
-        
+		<form action="" method="post" name="csv">
+ 		<input type= 'submit' name='csv' value='Salvar em CSV' />
+  		</form>
+		<?php
+
+		include ("con.php");
+
+		$ponto = $_POST['idPonto'];
+		$datainicio = $_POST['datainicio'];
+		$horainicio = $_POST['horainicio'];
+		$datafinal = $_POST['datafinal'];
+		$horafinal = $_POST['horafinal'];
+
 		if (isset($_POST['buscar'])) {
 			$operacao = 'buscar';
 		} else {
 			$operacao = 'csv';
-			header('Content-type: text/csv; charset=UTF-8; header=present');
+			header('Content-type: application/csv');
 			header('Content-Disposition: attachment; filename="pontos.csv"');
 			$arquivo = fopen('php://output', 'w');
-			fputcsv($arquivo, array("identificador", "hora", "data", "precipitacao", "latitude", "longitude"));			
-		}		
-		
-        $lista_pontos = explode(";", $ponto);
-        $tamanho = count($lista_pontos);
-        
-        for ($j = 0; $j < $tamanho; $j++) {
+			fputcsv($arquivo, array("ID", "hora", "data", "precipitacao", "latitude", "longitude"));
+		 
+		}
+		/*verificar se a data esta correta (isso nao ta fu<title>teste</title>ncionando :/
+		 list ($ano, $mes, $dia) = split ('[/.-]', $datainicio);
+		 list ($ano, $mes, $dia) = split ('[/.-]', $datafinal);
+		 if($ano>2011 && $ano<1998){
+		 echo "<script>alert('Ano invalido. ');</script>";
+		 }
+		 if($mes>12){
+		 echo "<script>alert('Mês invalido.');</script>";
+		 }
+		 if($dia>31){
+		 echo "<script>alert('Dia invalido. ');</script>";
+		 }
+		 */
 
-				if(isset($_POST['buscar'])){
-                	echo "<table border='1' style='border-collapse: collapse'>";
-                	echo "<tr> <td>Identificador</td> <td>Hora</td>        <td>Data</td> <td>Precipitacao</td> <td>Latitude</td>";
-                	echo "<td>Longitude</td></tr>";
-				}
-                        
-                $pg_con = new conexao();
-        
-                $teste = $pg_con -> CRUD('read', 'SELECT pontosfk, 
+		$lista_pontos = explode(";", $ponto);
+		$tamanho = count($lista_pontos);
+
+		for ($j = 0; $j < $tamanho; $j++) {
+
+			if (isset($_POST['buscar'])) {
+				echo "<table border='1' style='border-collapse: collapse'>";
+				echo "<tr> <td>Identificador</td> <td>Hora</td>        <td>Data</td> <td>Precipitacao</td> <td>Latitude</td>";
+				echo "<td>Longitude</td></tr>";
+			}
+
+			$pg_con = new conexao();
+
+			$teste = $pg_con -> CRUD('read', 'SELECT pontosfk, 
                                hora, 
                                data, 
                                precipitac, 
@@ -47,32 +64,34 @@
                                           AND hora <= ? 
                         ORDER  BY data, 
                           hora', array($lista_pontos[$j], $lista_pontos[$j], $datainicio, $datafinal, $horainicio, $horafinal));
-        
-                for ($i = 0; $i < count($teste); $i++) {
 
-						if(isset($_POST['buscar'])){
-	                        echo "<tr>";
-	                                
-	                        echo "<td>" . $teste[$i] -> pontosfk . "</td>";
-	                        echo "<td>" . $teste[$i] -> hora . "</td>";
-	                        echo "<td>" . $teste[$i] -> data . "</td>";
-	                        echo "<td>" . $teste[$i] -> precipitac . "</td>";
-	                        echo "<td>" . $teste[$i] -> latitude . "</td>";
-	                        echo "<td>" . $teste[$i] -> longitude . "</td>";
-	                                
-	                        echo "</tr>";							
-							
-						}else{
-							fputcsv($arquivo, array($teste[$i] -> pontosfk, $teste[$i] -> hora, $teste[$i] -> data, $teste[$i] -> precipitac, $teste[$i] -> latitude, $teste[$i] -> longitude));
-						}
-                }
-				
-				if(isset($_POST['buscar'])){
-                	echo "</table><br>";
+			for ($i = 0; $i < count($teste); $i++) {
+
+				if (isset($_POST['buscar'])) {
+					echo "<tr>";
+
+					echo "<td>" . $teste[$i] -> pontosfk . "</td>";
+					echo "<td>" . $teste[$i] -> hora . "</td>";
+					echo "<td>" . $teste[$i] -> data . "</td>";
+					echo "<td>" . $teste[$i] -> precipitac . "</td>";
+					echo "<td>" . $teste[$i] -> latitude . "</td>";
+					echo "<td>" . $teste[$i] -> longitude . "</td>";
+
+					echo "</tr>";
+
+				} else {
+					fputcsv($arquivo, array($teste[$i] -> pontosfk, $teste[$i] -> hora, $teste[$i] -> data, $teste[$i] -> precipitac, $teste[$i] -> latitude, $teste[$i] -> longitude));
+
 				}
-        }
+			}
 
-		if(isset($_POST['csv'])){
+			if (isset($_POST['buscar'])) {
+				echo "</table><br>";
+			}
+		}
+
+		if (isset($_POST['csv'])) {
 			fclose($arquivo);
 		}
-?>
+		
+	?>
--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 			}
 			#info {
 				position: absolute;
-				top: 6em;
+				top: 1em;
 				left: 700px;
 			}
 			#info table td {
@@ -48,23 +48,23 @@
 		</style>
 		<script defer="defer" type="text/javascript">
 			OpenLayers.ProxyHost = "/cgi-bin/proxy.cgi?url=";
-
-
-
-			
 			var map, infocontrols, highlightlayer;
 			function load() {
 				var bounds = new OpenLayers.Bounds(-61.875, -22.125, -50.2247507568401, -7.34904988281315);
-				var opcoes = {
+
+				map = new OpenLayers.Map('map', {
+					controls : [new OpenLayers.Control.Navigation(), new OpenLayers.Control.PanZoomBar(), new OpenLayers.Control.LayerSwitcher({
+						'ascending' : false
+					}), new OpenLayers.Control.ScaleLine(), new OpenLayers.Control.MousePosition(), new OpenLayers.Control.OverviewMap()],
+					numZoomLevels : 20,
 					maxExtent : bounds,
 					maxResolution : 0.025,
 					projection : "EPSG:4326",
-					};
 
-				map = new OpenLayers.Map('map', opcoes);
+				});
 
 				var pontos = new OpenLayers.Layer.WMS("Pontos", "http://localhost:8080/geoserver/trmm/wms", {
-					LAYERS : 'trmm:pontos'
+					LAYERS : 'trmm:MT,pontos'
 				}, {
 					buffer : 0,
 					displayOutsideMaxExtent : true,
@@ -100,32 +100,17 @@
 				};
 
 				map.addLayers([pontos, highlightLayer]);
-		 
+
 				for (var i in infoControls) {
 					infoControls[i].events.register("getfeatureinfo", this, showInfo);
 
 					map.addControl(infoControls[i]);
 				}
-				// Adicionando controles
-				map.addControl(new OpenLayers.Control.Navigation());
-				map.addControl(new OpenLayers.Control.LayerSwitcher({'ascending':false}));
-				map.addControl(new OpenLayers.Control.MousePosition());
-				map.addControl(new OpenLayers.Control.ScaleLine());
-				map.addControl(new OpenLayers.Control.OverviewMap());
-				map.addControl(new OpenLayers.Control.PanZoomBar());
-				map.addControl(new OpenLayers.Control.MousePosition());
-				map.addControl(new OpenLayers.Control.OverviewMap());
-				map.addControl(new OpenLayers.Control.KeyboardDefaults());
 
 				infoControls.click.activate();
-				
 
 				map.zoomToMaxExtent(bounds);
-				map.setCenter(new OpenLayers.LonLat(-57.625, -17.125).transform(
-                    new OpenLayers.Projection("EPSG:4326"),
-                    map.getProjectionObject()
-                    ));
-				
+				map.setCenter(new OpenLayers.LonLat(-12.625000000000000, -55.875000000000000).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()));
 
 			}
 
@@ -136,23 +121,37 @@
 					highlightLayer.addFeatures(evt.features);
 					highlightLayer.redraw();
 				} else {
-					
+					var mt = evt.text.split("<td>")[2].split("</td>")[0];
 					//Aqui a gente pega o que esta dentro da caixinha.
 					var parte1 = $("#gidbuscado").val();
-					
+                    document.getElementById('responseText').innerHTML = evt.text; 
 					//Depois que se clica num ponto, se quebra a resposta atras da chave.
 					var parte2 = evt.text.split("<td>")[2].split("</td>")[0];
-					
-					//Aqui a gente coloca dentro da caixinha.
-					if(parte1 == ""){
-						//se nao tiver nada na caixinha, ele coloca o ponto clicado
-						$("#gidbuscado").val(parte2);
-					}else{
-						//se ja tiver coisa na caixinha, ele pega o da caixinha e adiciona ponto e virgula e o novo.
-						$("#gidbuscado").val(parte1 + "; " + parte2);
+
+					if (mt == "1") {
+						var parte = evt.text.split("<td>")[15].split("</td>")[0];
+						//Aqui a gente coloca dentro da caixinha.
+						if (parte1 == "") {
+							//se nao tiver nada na caixinha, ele coloca o ponto clicado
+							$("#gidbuscado").val(parte);
+						} else {
+							//se ja tiver coisa na caixinha, ele pega o da caixinha e adiciona ponto e virgula e o novo.
+							$("#gidbuscado").val(parte1 + "; " + parte);
+						}
+
+						document.getElementById('responseText').innerHTML = evt.text;
+					} else {
+						//Aqui a gente coloca dentro da caixinha.
+						if (parte1 == "") {
+							//se nao tiver nada na caixinha, ele coloca o ponto clicado
+							$("#gidbuscado").val(parte2);
+						} else {
+							//se ja tiver coisa na caixinha, ele pega o da caixinha e adiciona ponto e virgula e o novo.
+							$("#gidbuscado").val(parte1 + "; " + parte2);
+						}
+
+						document.getElementById('responseText').innerHTML = evt.text;
 					}
-					
-					document.getElementById('responseText').innerHTML = evt.text;
 				}
 			}
 
@@ -192,16 +191,72 @@
 	</head>
 
 	<body onload="load()">
-		<h1 id="title">Gerenciador de dados TRMM</h1>
-
-		<p id="shortdesc">
-			Dados de precipitação.
-		</p>
 		<div id="info">
-			<br>
-			<br>
-			<br>
-			<br>
+			<h1 id="title">Gerenciador de dados TRMM - Dados de precipitação</h1>
+			<p id="shortdesc">
+				
+			</p>
+			<form target="_blank"  action="conecta.php" method="POST"  >
+				ID do ponto:
+				<p>
+					<textarea rows="2" id="gidbuscado" required name="idPonto" cols="63"></textarea>
+				</p>
+
+				Data de inicio:
+				<input type="text" required name="datainicio" size="10" value="AAAA-MM-DD" maxlength="10" onclick="this.value=''">
+				Data final:
+				<input type="text" required name="datafinal" size="10" value="AAAA-MM-DD" maxlength="10" onclick="this.value=''" >
+				<br>
+				Hora de inicio:
+				<input type="text" required name="horainicio" size="10">
+				Hora final:
+				<input type="text" required name="horafinal" size="10">
+				<br>
+				<input type="submit" value="Buscar" name="buscar">
+				
+				
+				<br>
+			</form>
+
+			<ul id="control">
+				<li>
+					<input type="radio" name="controlType" value="click" id="click"
+					onclick="toggleControl(this);" checked="checked" />
+					<label for="click">Click para mostrar os dados</label>
+				</li>
+				<li>
+					<input type="radio" name="controlType" value="hover" id="hover"
+					onclick="toggleControl(this);" />
+					<label for="hover">Passe o mouse para mostrar os dados</label>
+				</li>
+			</ul>
+			<ul id="format">
+				<li>
+					<input type="radio" name="formatType" value="text/html" id="html"
+					onclick="toggleFormat(this);" checked="checked" />
+					<label for="html">Mostrar dados</label>
+
+				</li>
+				<li>
+					<input type="radio" name="formatType" value="application/vnd.ogc.gml" id="highlight"
+					onclick="toggleFormat(this);" />
+					<label for="highlight">Destaque de recursos no mapa</label>
+				</li>
+			</ul>
+			<ul id="layers">
+				<li>
+					<input type="radio" name="layerSelection" value="Specified" id="Specified"
+					onclick="toggleLayers(this);" checked="checked" />
+					<label for="Specified">Informacoes dos pontos</label>
+				</li>
+
+				<li>
+					<input type="radio" name="layerSelection" value="Auto" id="Auto"
+					onclick="toggleLayers(this);" />
+					<label for="Auto">Informacoes gerais</label>
+				</li>
+			</ul>
+			
 			<h1>Trmm</h1>
 			<p>
 				Clique no mapa para obter informacoes.
@@ -210,67 +265,8 @@
 
 		</div>
 
-		<form target="_blank"  action="conecta.php" method="POST"  >
-
-			ID do ponto:
-			<input type="text" id="gidbuscado" required name="idPonto"/>
-			<br>
-			Data de inicio:
-			<input type="text" required name="datainicio">
-			Hora de inicio:
-			<input type="text" required name="horainicio">
-			<br>
-			Data final:
-			<input type="text" required name="datafinal">
-			Hora final:
-			<input type="text" required name="horafinal">
-			<br>
-			<input type="submit" value="Buscar" name="buscar">
-			<input type="submit" name="csv" value="Salvar em CSV" />
-			
-		</form>
-
 		<div id="map" class="smallmap"></div>
 		<div id="docs"></div>
-
-		<ul id="control">
-			<li>
-				<input type="radio" name="controlType" value="click" id="click"
-				onclick="toggleControl(this);" checked="checked" />
-				<label for="click">Click para mostrar os dados</label>
-			</li>
-			<li>
-				<input type="radio" name="controlType" value="hover" id="hover"
-				onclick="toggleControl(this);" />
-				<label for="hover">Passe o mouse para mostrar os dados</label>
-			</li>
-		</ul>
-		<ul id="format">
-			<li>
-				<input type="radio" name="formatType" value="text/html" id="html"
-				onclick="toggleFormat(this);" checked="checked" />
-				<label for="html">Mostrar dados</label>
-				
-			</li>
-			<li>
-				<input type="radio" name="formatType" value="application/vnd.ogc.gml" id="highlight"
-				onclick="toggleFormat(this);" />
-				<label for="highlight">Destaque de recursos no mapa</label>
-			</li>
-		</ul>
-		<ul id="layers">
-			<li>
-				<input type="radio" name="layerSelection" value="Specified" id="Specified"
-				onclick="toggleLayers(this);" checked="checked" />
-				<label for="Specified">Informacoes dos pontos</label>
-			</li>
-
-			<li>
-				<input type="radio" name="layerSelection" value="Auto" id="Auto"
-				onclick="toggleLayers(this);" />
-				<label for="Auto">Informacoes gerais</label>
-			</li>
-		</ul>
 
 	</body>
 </html>
